2bbb870416fa4a41fbbea1295ea47b547be1a16d
Ndavlo <nicolasdl99@gmail.com>
2022-12-04 23:26:13 +0000

    ExcuseGen

--- src/app.js
+++ src/app.js
@@ -6,6 +6,45 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let generateExcuse = () => {
+    let who = [
+      "The dog",
+      "My grandma",
+      "His turtle",
+      "My bird",
+      "My snake",
+      "My crocodile"
+    ];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying",
+      "before I could get in the car",
+      "before the presentation",
+      "before I arrived"
+    ];
+
+    let whoI = Math.floor(Math.random() * who.length);
+    let actionI = Math.floor(Math.random() * action.length);
+    let whatI = Math.floor(Math.random() * what.length);
+    let whenI = Math.floor(Math.random() * when.length);
+
+    return (
+      who[whoI] +
+      " " +
+      action[actionI] +
+      " " +
+      what[whatI] +
+      " " +
+      when[whenI] +
+      "."
+    );
+  };
+  document.querySelector("#buttonGen").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
 };
